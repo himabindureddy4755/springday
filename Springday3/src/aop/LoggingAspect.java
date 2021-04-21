030d87357fa2574e475f976642faaeb2cee2d2d3
hp <bindureddy4755@gmail.com>
2021-04-21 14:16:52 +0530

    commit

--- Springday3/src/aop/LoggingAspect.java
+++ Springday3/src/aop/LoggingAspect.java
@@ -6,16 +6,62 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
-	//@Before("allGetters()")
+	//one aspect can contain multiple advices
 
-	//@Before("execution(public String aop.Triangle.getName())")
-	@Before("execution(public * aop.*.get*())")   
-	public void loggingAdvice() {
-		System.out.println("writing log before method is executed");
-	}
-	
-	
-	//@Pointcut("execution(* aop..*.get*())")
-	//public void allGetters() {}
+		//@Before("execution(* aop..*.*(..))")   --- for all methods of all classes present in this package and its subpackages
+
+		//@Before("execution(public String aop.Triangle.getName())")
+		//@Before("execution(* aop..*.*(..))")  --for aop and its subpackages
+
+		//@Before("execution(* aop..*.*(..))")
+
+		//@Before("execution(public String aop.*.getName())")  --- for all getName methods in all classes within the package aop.model
+		//@Before("execution(public * aop.*.getName())")  --- irrespective of return type
+		//@Before("execution(public * aop.*.get*())")   --- for all methods which start with get
+
+
+		//@Before("execution(* aop..*.get*())")  == 	@Before("allGetters()")
+
+		/*
+		 * @Before("allCircleMethods()") public void loggingAdvice(JoinPoint joinPoint)
+		 * { String methodName = joinPoint.toLongString();
+		 * 
+		 * if(methodName.contains("getDia")) { Circle circle = (Circle)
+		 * joinPoint.getTarget(); System.out.println(circle.getName());
+		 * System.out.println("writing log for getdia method b4 its executed");
+		 * 
+		 * } else if(methodName.contains("setName")) {
+		 * System.out.println("writing log for setName method b4 its executed");
+		 * 
+		 * 
+		 * } }
+		 */
+
+		@Pointcut("execution(* aop..*.get*())")
+		public void allGetters() {}
+
+		@Pointcut("within(aop.Circle)")
+		public void allCircleMethods() {}
+		
+		//@Before("args(name)")
+		@Before("methodsStringArgs(name)")
+		public void stringArgsAdvice(String name) {
+			System.out.println("advice for all methods which accept a string as an argument "+name);
+		}
+
+		@Pointcut("args(name)")
+		public void methodsStringArgs(String name) {}
+
+
+		/*
+		 * @Before("execution(* aop..*.get*())") public void secondAdviceforAllGetters()
+		 * { System.out.println("second log before method is executed"); }
+		 * 
+		 * //@Before("execution(* aop..*.get*())")
+		 * 
+		 * @Before("allGetters") public void thirdAdviceforAllGetters() {
+		 * System.out.println("third log before method is executed"); }
+		 */
+
 
 }
---
+++ Springday3/src/aop/ShapeService.java
@@ -0,0 +1,20 @@
+package aop;
+
+public class ShapeService {
+	Circle circle;
+	Triangle triangle;
+	
+	public Circle getCircle() {
+		return circle;
+	}
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+	public Triangle getTriangle() {
+		return triangle;
+	}
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+
+}
