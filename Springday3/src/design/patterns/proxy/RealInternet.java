cc4fc4499a4c70debf1c7c91e9f14c4aa4be7147
hp <bindureddy4755@gmail.com>
2021-04-22 11:32:50 +0530

    aop proxy design patterns

---
+++ Springday3/src/design/patterns/proxy/RealInternet.java
@@ -0,0 +1,9 @@
+package design.patterns.proxy;
+public class RealInternet implements Internet
+{
+	@Override
+	public void connectTo(String serverhost)
+	{
+		System.out.println("Connecting to "+ serverhost);
+	}
+}
